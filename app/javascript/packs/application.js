768c99d1618c4ea80a942554045bdd216de45569
matariyashantilal <shantilal13@gmail.com>
2020-09-13 21:54:40 +0530

    add calling images in app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("channels")
 // import '../packs/jquery.js'
 // import '../packs/jquery.min.js'
 // import '../packs/marriage.js'
+const importAll = (r) => r.keys().map(r)
+importAll(require.context('../images', false, /\.(png|svg|gif|jpg|eot|ttf|woff|woff2)$/));
 
 
 
@@ -18,5 +20,8 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+window.jQuery = $;
+window.$ = $;
