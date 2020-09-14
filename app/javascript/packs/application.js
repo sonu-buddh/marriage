c139d2f8325bc5380ffa5451a89898c66138913a
matariyashantilal <shantilal13@gmail.com>
2020-09-14 09:07:00 +0530

    add style folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import '../stylesheets/application'
+import "../packs/jquery.js"
+
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -14,8 +18,6 @@ require("channels")
 const importAll = (r) => r.keys().map(r)
 importAll(require.context('../images', false, /\.(png|svg|gif|jpg|eot|ttf|woff|woff2)$/));
 
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -23,5 +25,5 @@ importAll(require.context('../images', false, /\.(png|svg|gif|jpg|eot|ttf|woff|w
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
-window.jQuery = $;
-window.$ = $;
+// window.jQuery = $;
+// window.$ = $;
